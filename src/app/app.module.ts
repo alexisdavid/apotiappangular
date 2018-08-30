4939f56175923343145a6f48508c6aa94e83678a
alexisdavid <alexisduarte1512@gmail.com>
2018-08-29 22:10:08 -0500

    se anaden componentes home,search,navbar,footer y se agregan rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+//routes
+import { APP_ROUTING } from './app.routes';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
